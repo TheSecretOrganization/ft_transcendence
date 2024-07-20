3c436bfbbed595b7bf0774eba44b04bca92e9301
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-07-20 16:10:55 +0200

    wip

--- django/src/ft_transcendence/urls.py
+++ django/src/ft_transcendence/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from django.views.generic import TemplateView
+from pong.views import MyModelViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'mymodels', MyModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='myapp/index.html')),
 ]
---
+++ django/src/pong/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import MyModel
+from .serializers import MyModelSerializer
+
+class MyModelViewSet(viewsets.ModelViewSet):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
