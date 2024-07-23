f4eeeca313403bbc24dc11e7035c7c41a46b7ed3
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-07-23 14:16:00 +0200

    wip

--- django/src/ft_transcendence/urls.py
+++ django/src/ft_transcendence/urls.py
@@ -2,7 +2,7 @@
 URL configuration for ft_transcendence project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,17 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from django.views.generic import TemplateView
-from pong.views import MyModelViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'mymodels', MyModelViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('', TemplateView.as_view(template_name='myapp/index.html')),
 ]
--- django/src/pong/views.py
+++ django/src/pong/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import MyModel
-from .serializers import MyModelSerializer
 
-class MyModelViewSet(viewsets.ModelViewSet):
-    queryset = MyModel.objects.all()
-    serializer_class = MyModelSerializer
+# Create your views here.
