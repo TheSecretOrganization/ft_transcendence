c879dbe53a9ced7fcc018e3dc5285feb3802d60b
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-07-25 05:19:01 +0200

    added media folder

--- django/src/ft_transcendence/settings.py
+++ django/src/ft_transcendence/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
