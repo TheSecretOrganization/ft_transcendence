8ac5922ff9862c3a9ebb0dca4682ac3799eca1d4
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-07-27 03:15:49 +0200

    renamed api in rendering and moved all api endpoint in main router

--- django/src/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FetchPageView
-
-urlpatterns = [
-    path('fetch-page/<str:page_name>/', FetchPageView.as_view(), name='fetch_page'),
-]
--- django/src/ft_transcendence/urls.py
+++ django/src/ft_transcendence/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from rendering.views import FetchPageView
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include(router.urls)),
+    path('api/fetch-page/<str:page_name>/', FetchPageView.as_view(), name='fetch_page'),
 ]
