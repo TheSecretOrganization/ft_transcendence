b10758f987eb57fb5a184e6eaab4958fac50bef9
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-08-06 11:09:19 +0200

    updated database to hardcode some params

--- django/src/ft_transcendence/settings.py
+++ django/src/ft_transcendence/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'ft_transcendence.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": os.getenv("POSTGRES_ENGINE", "django.db.backends.sqlite3"),
+        "ENGINE": os.getenv("django.db.backends.postgresql_psycopg2"),
         "NAME": os.getenv("POSTGRES_DB", BASE_DIR / "db.sqlite3"),
         "USER": os.getenv("POSTGRES_USER", "user"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
-        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "HOST": os.getenv("postgres"),
+        "PORT": os.getenv("5432"),
     }
 }
 
