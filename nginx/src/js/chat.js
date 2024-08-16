cda02b2683ef533d1a1208fca642cf022005f8bf
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-08-17 00:13:17 +0200

    added multiples rooms to chat

--- nginx/src/js/chat.js
+++ nginx/src/js/chat.js
@@ -1,31 +1,89 @@
-const roomName = "general";
-const chatSocket = new WebSocket(`wss://${window.location.host}/ws/chat/${roomName}/`);
 const chatLog = document.getElementById('chat-log');
 const chatInput = document.getElementById('chat-input');
+const chatRooms = document.getElementById('chat-list');
+let currentRoom = "general";
+let chatSocket;
+let isClosing = false;
 
-chatSocket.onmessage = function(e) {
-    const data = JSON.parse(e.data);
-    chatLog.value += (data.message + '\n');
-};
-
-chatSocket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
-
-chatInput.onkeyup = function(e) {
-    if (e.key === 'Enter' && chatInput.value.length !== 0) {
-        chatSocket.send(JSON.stringify({
-            'message': chatInput.value
-        }));
-        chatInput.value = '';
+function updateActiveRoom() {
+    document.querySelectorAll('#chat-list .list-group-item').forEach(item => {
+        item.classList.remove('active');
+    });
+    const activeRoom = document.querySelector(`[data-room="${currentRoom}"]`);
+    if (activeRoom) {
+        activeRoom.classList.add('active');
     }
-};
+}
+
+function switchRoom(roomName) {
+    if (chatSocket) {
+        isClosing = true;
+        chatSocket.close();
+    }
+
+    currentRoom = roomName;
+    updateActiveRoom();
+    chatLog.value = "";
+
+    chatSocket = new WebSocket(`wss://${window.location.host}/ws/chat/${roomName}/`);
+
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        chatLog.value += (data.message + '\n');
+    };
+
+    chatSocket.onclose = function(e) {
+        if (!isClosing) {
+            console.error('Chat socket closed unexpectedly');
+        }
+        isClosing = false;
+    };
+
+    chatInput.onkeyup = function(e) {
+        if (e.key === 'Enter' && chatInput.value.length !== 0) {
+            chatSocket.send(JSON.stringify({
+                'message': chatInput.value
+            }));
+            chatInput.value = '';
+        }
+    };
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    chatLog.value = ""
+    switchRoom(currentRoom);
 });
 
-/* Chat Toggle */
+chatRooms.addEventListener('click', (e) => {
+    if (e.target && e.target.hasAttribute('data-room')) {
+        e.preventDefault;
+        switchRoom(e.target.getAttribute('data-room'));
+    }
+});
+
+// fetch('/api/chatrooms')
+//     .then(response => response.json())
+//     .then(rooms => {
+//         rooms.forEach(room => {
+//             const li = document.createElement('li');
+//             li.className = 'list-group-item';
+//             li.setAttribute('data-room', room.name);
+//             li.innerText = room.displayName;
+//             chatRooms.appendChild(li);
+//         });
+
+//         // Reinitialize the click event listeners after loading
+//         chatRooms.addEventListener('click', (e) => {
+//             if (e.target && e.target.nodeName === "LI") {
+//                 const roomName = e.target.getAttribute('data-room');
+//                 document.querySelectorAll('#chat-list .list-group-item').forEach(item => {
+//                     item.classList.remove('active');
+//                 });
+//                 e.target.classList.add('active');
+//                 switchRoom(roomName);
+//             }
+//         });
+//     });
+
 document.getElementById('chat-toggle').onclick = function(e) {
     e.preventDefault();
     document.getElementById('chat-wrapper').classList.toggle("toggled");
--- nginx/src/js/router.js
+++ nginx/src/js/router.js
@@ -1,7 +1,6 @@
-/* Router */
-function route(event) {
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
+function route(e) {
+    e.preventDefault();
+    window.history.pushState({}, "", e.target.getAttribute('data-route'));
     handleLocation();
 };
 
@@ -35,6 +34,26 @@ function loadScripts() {
     });
 };
 
+function updateTitle(pageName) {
+    const titles = {
+        "404": "Page Not Found",
+        "index": "Home",
+        "games": "Games",
+    };
+    const title = titles[pageName] || "Page Not Found";
+    document.title = title;
+};
+
+function updateActiveRoute(path) {
+    document.querySelectorAll('#sidebar-list .list-group-item').forEach(item => {
+        item.classList.remove('active');
+    });
+    const activeRoute = document.querySelector(`[data-route="${path}"]`);
+    if (activeRoute) {
+        activeRoute.classList.add('active');
+    }
+}
+
 async function handleLocation() {
     let path = window.location.pathname;
     let pageName = path === '/' ? 'index' : path.substring(1);
@@ -46,32 +65,21 @@ async function handleLocation() {
     document.getElementById("page-container").innerHTML = html;
     loadScripts();
     updateTitle(pageName);
-};
-
-function updateTitle(pageName) {
-    const titles = {
-        "404": "Page Not Found",
-        "index": "Home",
-        "games": "Games",
-    };
-    const title = titles[pageName] || "Page Not Found";
-    document.title = title;
+    updateActiveRoute(path);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     window.onpopstate = handleLocation;
     window.route = route;
     handleLocation();
-
-    document.addEventListener("click", (event) => {
-        if (event.target.matches("a[data-route]")) {
-            route(event);
-        }
-    });
 });
 
+document.addEventListener("click", (e) => {
+    if (e.target && e.target.hasAttribute('data-route')) {
+        route(e);
+    }
+});
 
-/* Sidebar Toggle */
 document.getElementById("sidebar-toggle").addEventListener("click", function (e) {
     e.preventDefault();
     document.getElementById("sidebar-wrapper").classList.toggle("toggled");
