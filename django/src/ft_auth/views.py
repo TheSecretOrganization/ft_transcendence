0ed1f4c5abc95a114309d83ff73ec23486a78931
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-08-24 20:27:07 +0200

    add logic for login and logout

--- django/src/ft_auth/views.py
+++ django/src/ft_auth/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponseBadRequest, HttpResponse, HttpRequest, JsonResponse
+from django.contrib.auth import authenticate
 
-# Create your views here.
+def login(request: HttpRequest):
+	if all(k not in request.POST for k in ('username', 'password')):
+		return HttpResponseBadRequest({'message': 'Missing fields (required username and password)'})
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is None:
+		return JsonResponse({'message': 'Wrong credentials'}, status=401)
+	request.session['user_id'] = user.id
+	return HttpResponse(status=200)
+
+def logout(request: HttpRequest):
+	if 'user_id' in request.session:
+		del request.session['user_id']
+		return HttpResponse(status=200)
+	else:
+		return JsonResponse({'message': 'You\'re not logged in'}, status=401)
