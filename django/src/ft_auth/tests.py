e230e196249ec6d6c9dc245079dd24540b3a876e
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-08-25 17:51:29 +0200

    remove unused import in auth tests

--- django/src/ft_auth/tests.py
+++ django/src/ft_auth/tests.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.test import Client, TestCase
-from django.contrib.sessions.models import Session
 
 class UserManagerTest(TestCase):
 
