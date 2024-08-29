d29cbdfb7bccd46715ec6279a879563b4c32e912
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-08-29 17:21:11 +0200

    removed drf urls

--- django/src/core/urls.py
+++ django/src/core/urls.py
@@ -16,12 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
-router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
 ]
