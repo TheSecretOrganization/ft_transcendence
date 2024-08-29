c003a386c929c2b5287cb5addeabaf093a476a73
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-08-29 16:20:22 +0200

    add route for index and games pages

--- django/src/pages/views.py
+++ django/src/pages/views.py
@@ -1,3 +1,10 @@
+from django.views.decorators.http import require_GET
 from django.shortcuts import render
 
-# Create your views here.
+@require_GET
+def index(request):
+	return render(request, 'index.html')
+
+@require_GET
+def games(request):
+	return render(request, 'games.html')
