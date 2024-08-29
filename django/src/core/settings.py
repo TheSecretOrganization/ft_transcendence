c7edb629610a3a6da98919cb42a7ff04ec7ba585
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-08-29 17:20:55 +0200

    removed drf settings

--- django/src/core/settings.py
+++ django/src/core/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'tools',
     'pages',
 ]
@@ -147,8 +146,3 @@ MEDIA_ROOT = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
