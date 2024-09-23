55faa0bbe898e222bddd1965d7feeae8ac0d7495
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-09-23 22:39:52 +0200

    add Pong to admin page

--- django/src/games/admin.py
+++ django/src/games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pong
 
-# Register your models here.
+@admin.register(Pong)
+class PongAdmin(admin.ModelAdmin):
+    list_display = ('user1', 'user2', 'score1', 'score2', 'created_at')
+    search_fields = ('user1__username', 'user2__username')
+    list_filter = ('created_at',)
