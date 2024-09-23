206ed523aa76a5f1f8d4809b03383d94605cda75
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-09-23 13:28:26 +0200

    refactor getCookie function in core

--- nginx/src/js/core.js
+++ nginx/src/js/core.js
@@ -31,9 +31,8 @@ function submitForm(id, oncomplete, onerror) {
 function getCookie(name) {
 	let cookieValue = null;
 	if (document.cookie && document.cookie !== '') {
-		const cookies = document.cookie.split(';');
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = cookies[i].trim();
+		const cookies = document.cookie.split(';').map(cookie => cookie.trim());
+		for (let cookie of cookies) {
 			if (cookie.substring(0, name.length + 1) === (name + '=')) {
 				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 				break;
