47088b27bc4196bb7fae3b15b1ad07222441573c
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-09-25 14:30:42 +0200

    update Pong Model

--- django/src/games/models.py
+++ django/src/games/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from ft_auth.models import User
+
 
 class Pong(models.Model):
-    user1 = models.ForeignKey("ft_auth.User", on_delete=models.SET_NULL, null=True, related_name='user1')
-    user2 = models.ForeignKey("ft_auth.User", on_delete=models.SET_NULL, null=True, related_name='user2')
+    user1 = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="user1"
+    )
+    user2 = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="user2"
+    )
     score1 = models.PositiveSmallIntegerField()
     score2 = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user1.username} vs {self.user2.username} - {self.score1}:{self.score2}'
+        return f"{self.user1.username} vs {self.user2.username} - {self.score1}:{self.score2}"
