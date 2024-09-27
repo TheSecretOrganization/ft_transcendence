2667dba32282138dd3c9a3bd211bf973ee6e31e6
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-09-27 13:06:56 +0200

    add Friend model

--- django/src/friends/models.py
+++ django/src/friends/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from ft_auth.models import User
 
-# Create your models here.
+class Friend(models.Model):
+	class Status(models.IntegerChoices):
+		PENDING = 1
+		ACCEPTED = 2
+		DENIED = 3
+
+	origin = models.ForeignKey(User, models.CASCADE, related_name='origin')
+	target = models.ForeignKey(User, models.CASCADE, related_name='target')
+	status = models.IntegerField(choices=Status, default=Status.PENDING)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(fields=['origin', 'target'], name='unique_friend_request'),
+			models.CheckConstraint(check=~models.Q(origin=models.F('target')), name='unique_origin_target')
+		]
+
+	def clean(self):
+		if Friend.objects.filter(origin=self.target, target=self.origin).exists():
+			raise ValidationError('This request already exist')
+
+	def save(self, *args, **kwargs):
+		self.clean()
+		super().save(*args, **kwargs)
