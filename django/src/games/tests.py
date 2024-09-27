6c4219c9041b2cfbbaa5310b3570639899960527
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-09-27 15:36:24 +0200

    add anonymous connection and missing params connection tests to Pong

--- django/src/games/tests.py
+++ django/src/games/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from channels.testing import WebsocketCommunicator
+from core.asgi import application
+from asgiref.sync import sync_to_async
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class PongTest(TestCase):
+	async def test_anonymous_connection(self):
+		communicator = WebsocketCommunicator(application, "/ws/pong/")
+		connected, subprotocol = await communicator.connect()
+		self.assertFalse(connected)
+		await communicator.disconnect()
+
+	async def test_connection_missing_params(self):
+		test_user = await sync_to_async(get_user_model().objects.create_user)(
+        	username="testuser", password="testpassword"
+    	)
+		communicator = WebsocketCommunicator(application, "/ws/pong/")
+		communicator.scope["user"] = test_user
+		connected, subprotocol = await communicator.connect()
+		self.assertFalse(connected)
+		await communicator.disconnect()
