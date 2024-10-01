80f00d62e001340bf43563438d8e93cd6520b371
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-10-01 13:34:51 +0200

    create PongTournament model

--- django/src/games/models.py
+++ django/src/games/models.py
@@ -14,3 +14,15 @@ class Pong(models.Model):
 
     def __str__(self):
         return f"{self.user1.username} vs {self.user2.username} - {self.score1}:{self.score2}"
+
+class PongTournament(models.Model):
+    name = models.CharField(max_length=255)
+    participants = models.ManyToManyField(get_user_model(), related_name='tournaments')
+    games = models.ManyToManyField(Pong, related_name='tournaments')
+    current_round = models.PositiveSmallIntegerField(default=1)
+    max_rounds = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Tournament: {self.name} (Round {self.current_round}/{self.max_rounds})"
