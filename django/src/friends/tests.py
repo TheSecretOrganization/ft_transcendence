51b5a4b739ccf3d8a40e0843277486982392f2c5
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-10-02 11:49:33 +0200

    add tests for accept and deny friend invite

--- django/src/friends/tests.py
+++ django/src/friends/tests.py
@@ -93,3 +93,81 @@ class AddFriendTest(TestCase):
 		self.assertEqual(request.status_code, 400, "Unknown target didn't result in a 400")
 		self.assertFalse(Friend.objects.filter(origin=self.user, target=404).exists(),
 				   "Self invite created an entry in database")
+
+class AcceptFriendTest(TestCase):
+	def setUp(self):
+		self.route = '/friends/accept/'
+		self.user = get_user_model().objects.create_user(username='testuser', password='password')
+		self.user2 = get_user_model().objects.create_user(username='testuser2', password='password')
+		self.client.login(username='testuser', password='password')
+		self.invite = Friend.objects.create(origin=self.user2, target=self.user)
+
+	def test_accept_invite_success(self):
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 200)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.ACCEPTED)
+
+	def test_accept_invite_not_authenticated(self):
+		self.client.logout()
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 401)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.PENDING)
+
+	def test_accept_invite_missing_arguments(self):
+		response = post(self.client, self.route, {})
+		self.assertEqual(response.status_code, 400)
+		response = post(self.client, self.route, {'id_invite': 1})
+		self.assertEqual(response.status_code, 400)
+
+	def test_accept_invite_does_not_exist(self):
+		response = post(self.client, self.route, {'invite_id': 999})
+		self.assertEqual(response.status_code, 400)
+	
+	def test_accept_already_denied_invite(self):
+		self.invite.status = Friend.Status.DENIED
+		self.invite.save()
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 400)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.DENIED)
+
+class DenyFriendTest(TestCase):
+	def setUp(self):
+		self.route = '/friends/deny/'
+		self.user = get_user_model().objects.create_user(username='testuser', password='password')
+		self.user2 = get_user_model().objects.create_user(username='testuser2', password='password')
+		self.client.login(username='testuser', password='password')
+		self.invite = Friend.objects.create(origin=self.user2, target=self.user)
+
+	def test_deny_invite_success(self):
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 200)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.DENIED)
+
+	def test_deny_invite_not_authenticated(self):
+		self.client.logout()
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 401)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.PENDING)
+
+	def test_deny_invite_missing_arguments(self):
+		response = post(self.client, self.route, {})
+		self.assertEqual(response.status_code, 400)
+		response = post(self.client, self.route, {'id_invite': 1})
+		self.assertEqual(response.status_code, 400)
+
+	def test_deny_invite_does_not_exist(self):
+		response = post(self.client, self.route, {'invite_id': 999})
+		self.assertEqual(response.status_code, 400)
+
+	def test_deny_already_accepted_invite(self):
+		self.invite.status = Friend.Status.ACCEPTED
+		self.invite.save()
+		response = post(self.client, self.route, {'invite_id': self.invite.id})
+		self.assertEqual(response.status_code, 400)
+		self.invite.refresh_from_db()
+		self.assertEqual(self.invite.status, Friend.Status.ACCEPTED)
