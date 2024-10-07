7f23ea9467cf4ce8983136f65f743e448ee05fe0
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-10-07 07:55:51 +0200

    add admin for friends

--- django/src/friends/admin.py
+++ django/src/friends/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Friend
 
-# Register your models here.
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+	list_display = ('origin', 'target', 'status', 'created_at', 'updated_at')
+	search_fields = ('origin', 'target')
+	list_filter = ('created_at', 'updated_at', 'status')
