91ce3525ebe394afb8aaaec791a66e477828ab6a
Antoine <24255551+antoineverin@users.noreply.github.com>
2024-10-07 13:28:22 +0200

    load oauth when ft_auth is ready

--- django/src/ft_auth/apps.py
+++ django/src/ft_auth/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class FtAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ft_auth'
+
+    def ready(self) -> None:
+        from . import oauth
+        return super().ready()
