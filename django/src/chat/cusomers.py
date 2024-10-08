a19265cd29dda38c1baa737e4c74b27859b009f7
traccurt <traccurt@student.42.fr>
2024-10-08 16:07:06 +0200

    fix: bug double message + history

--- django/src/chat/cusomers.py
+++ django/src/chat/cusomers.py
@@ -15,38 +15,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
-
-        messages = await self.redis.lrange(f'chat_{self.room_group_name}_messages', 0, -1)
-
-        for message in messages:
-            await self.send(text_data=message)
-
-
-
+        
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         await self.redis.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = self.scope['user'].username if self.scope['user'].is_authenticated else 'Anonyme'
+        if text_data_json.get('type') == 'fetch_history':
+            await self.send_chat_history()
+        else:
+            message = text_data_json['message']
+            username = self.scope['user'].username if self.scope['user'].is_authenticated else 'Anonyme'
 
-        formatted_message = json.dumps({
-            'message': message,
-            'username': username
-        })
-
-        await self.redis.rpush(f'chat_{self.room_group_name}_messages', formatted_message)
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
+            formatted_message = json.dumps({
                 'message': message,
-                'username': username,
-            }
-        )
+                'username': username
+            })
+
+            await self.redis.rpush(f'chat_{self.room_group_name}_messages', formatted_message)
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'username': username,
+                }
+            )
+    
+    async def send_chat_history(self):
+        messages = await self.redis.lrange(f'chat_{self.room_group_name}_messages', 0, -1)
+        history = [json.loads(message) for message in messages]
+
+        await self.send(text_data=json.dumps({
+            'type': 'chat_history',
+            'messages': history
+        }))
 
     async def chat_message(self, event):
         message = event['message']
