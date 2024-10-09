8cdc90e6e50a552f0fd369ed7755f1082b710be9
traccurt <traccurt@student.42.fr>
2024-10-09 17:39:12 +0200

    add private chat + fix general

--- django/src/chat/consumers.py
+++ django/src/chat/consumers.py
@@ -7,17 +7,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.username = self.scope['user'].username if self.scope['user'].is_authenticated else 'Anonyme'
         self.redis = redis.Redis(host="redis", decode_responses=True)
 
-        if self.scope['url_route']['kwargs'].get('chat_type', 'General') == 'General':
-            self.room_group_name = 'chat_general'
-        else:
+        if self.scope['url_route']['kwargs'].get('username'):
             other_username = self.scope['url_route']['kwargs']['username']
-            self.room_group_name = f'private_chat_{self.username}_{other_username}'
+            users = sorted([self.username, other_username])
+            self.room_group_name = f'private_chat_{users[0]}_{users[1]}'
+        else:
+            self.room_group_name = 'chat_general'
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.add_active_user()
         await self.accept()
+        await self.broadcast_active_users()
+
+        if self.scope['url_route']['kwargs'].get('username'):
+            await self.notify_other_user(other_username)
         
     async def disconnect(self, close_code):
+        await self.remove_active_user()
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.broadcast_active_users()
         await self.redis.close()
 
     async def receive(self, text_data):
@@ -43,6 +51,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'username': username,
                 }
             )
+        
+    async def notify_other_user(self, other_username):
+        other_user_channel = f'user_{other_username}'
+        await self.channel_layer.group_send(
+            other_user_channel,
+            {
+                'type': 'private_message_notification',
+                'from_user': self.username,
+            }
+        )
     
     async def send_chat_history(self):
         messages = await self.redis.lrange(f'chat_{self.room_group_name}_messages', 0, -1)
@@ -60,4 +78,34 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message,
             'username': username
+        }))
+
+    async def private_message_notification(self, event):
+        from_user = event['from_user']
+        await self.send(text_data=json.dumps({
+            'type': 'private_message_notification',
+            'from_user': from_user,
+        }))
+
+    async def add_active_user(self):
+        await self.redis.sadd('active_users', self.username)
+
+    async def remove_active_user(self):
+        await self.redis.srem('active_users', self.username)
+
+    async def broadcast_active_users(self):
+        active_users = await self.redis.smembers('active_users')
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_active_users',
+                'users': list(active_users),
+            }
+        )
+
+    async def send_active_users(self, event):
+        users = event['users']
+        await self.send(text_data=json.dumps({
+            'type': 'active_users',
+            'users': users
         }))
--- django/src/chat/routing.py
+++ django/src/chat/routing.py
@@ -1,9 +1,7 @@
 from django.urls import re_path
-from .cusomers import ChatConsumer
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    # Route pour la salle générale
     re_path(r'ws/chat/General/$', ChatConsumer.as_asgi()),
-    # Route pour les chats privés entre utilisateurs
     re_path(r'ws/chat/private/(?P<username>\w+)/$', ChatConsumer.as_asgi()),
 ]
