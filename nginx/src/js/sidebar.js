c7d1d28d0f828d524794b552d87820630ef214b1
croueAlec <alec.croue@gmail.com>
2024-10-10 17:49:17 +0200

    when collapsed the sidebar's sub-menus can't be interacted with when tabbing

--- nginx/src/js/sidebar.js
+++ nginx/src/js/sidebar.js
@@ -1,14 +1,33 @@
+function subMenuInteractivity(submenu) {
+	const submenuLinks = submenu.querySelectorAll('a');
+
+	if (submenu.classList.contains('show')) {
+		submenuLinks.forEach(link => {
+			link.removeAttribute('tabindex');
+		});
+	} else {
+		submenuLinks.forEach(link => {
+			link.setAttribute('tabindex', '-1');
+		});
+	}
+}
+
 function toggleSubMenu(button) {
-	if (document.getElementById("sidebar").classList.contains('close')) {
-		document.getElementById("sidebar").classList.remove('close');
+	const sidebar = document.getElementById('sidebar');
+	const submenu = button.nextElementSibling;
+
+	if (sidebar.classList.contains('close')) {
+		sidebar.classList.remove('close');
 		document.getElementById("toggle-btn").classList.toggle('rotate');
 
-		document.getElementById("sidebar").addEventListener("transitionend", () => {
-			button.nextElementSibling.classList.toggle('show')
-			button.classList.toggle('rotate')
-		}, { once : true });
+		sidebar.addEventListener("transitionend", () => {
+			submenu.classList.toggle('show');
+			button.classList.toggle('rotate');
+			subMenuInteractivity(submenu);
+		}, { once: true });
 	} else {
-		button.nextElementSibling.classList.toggle('show')
-		button.classList.toggle('rotate')
+		submenu.classList.toggle('show');
+		button.classList.toggle('rotate');
+		subMenuInteractivity(submenu);
 	}
 }
