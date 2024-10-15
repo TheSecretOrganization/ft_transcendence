c34d4fc87b28f36f520dc1430448459bc053b174
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-10-15 19:11:21 +0200

    fix import

--- django/src/games/routing.py
+++ django/src/games/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from pong import Pong
-from tournament import Tournament
+from .pong import Pong
+from .tournament import Tournament
 
 websocket_urlpatterns = [
     re_path(r'ws/pong/?$', Pong.as_asgi()),
