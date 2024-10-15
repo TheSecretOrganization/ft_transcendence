801d5617a7d7000b4c44b87d747abdd8c473639d
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-10-15 18:08:25 +0200

    fix MEDIA_ROOT

--- django/src/core/settings.py
+++ django/src/core/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = '/static/'
 
 # Media files (User-uploaded content)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = '/app/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
