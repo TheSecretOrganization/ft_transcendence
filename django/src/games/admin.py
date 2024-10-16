bab5ee247ec7bf0ae6565e6204822720236f6c60
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-10-16 17:15:23 +0200

    admin page done

--- django/src/games/admin.py
+++ django/src/games/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Pong
+from .models import Pong, PongTournament
 
 
 @admin.register(Pong)
@@ -7,3 +7,10 @@ class PongGameAdmin(admin.ModelAdmin):
     list_display = ("user1", "user2", "score1", "score2", "created_at", "uuid")
     search_fields = ("user1__username", "user2__username")
     list_filter = ("created_at",)
+
+
+@admin.register(PongTournament)
+class PongTournamentAdmin(admin.ModelAdmin):
+    list_display = ("name", "winner", "created_at", "updated_at")
+    search_fields = ("name", "winner__username")
+    list_filter = ("created_at", "updated_at", "winner")
